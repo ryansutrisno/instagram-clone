6a9bfd96e5db8e2eff912727b9c50092d1f65532
Ryan Sutrisno <ryan.sutrisno@gmail.com>
2021-02-18 23:44:45 +0700

    create UI login and register

--- client/src/screens/Register/index.js
+++ client/src/screens/Register/index.js
@@ -1,6 +1,25 @@
+import {Link} from 'react-router-dom'
+
 const Register = () => {
     return (
-        <h1>Register</h1>
+        <>
+      <div className="mycard">
+        <div className="card auth-card input-field">
+        <h2>Instaxgram</h2>
+          <form>
+            <input type="text" placeholder="name" />
+            <input type="text" placeholder="email" />
+            <input type="password" placeholder="password" />
+            <button
+              className="btn waves-effect waves-light #64b5f6 blue lighten-2"
+            >
+              Register
+            </button>
+          </form>
+          <p><Link to="/login">Already have an account ?</Link></p>
+        </div>
+      </div>
+      </>
     )
 }
 
