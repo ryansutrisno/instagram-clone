a0892abc2d3873249449b9743bce0154b117e113
Ryan Sutrisno <ryan.sutrisno@gmail.com>
2021-03-19 18:22:05 +0700

    change bug fix

--- middleware/requireLogin.js
+++ middleware/requireLogin.js
@@ -1,5 +1,5 @@
 const jwt = require('jsonwebtoken')
-const {JWTSECRET} = require('../config/keys')
+const {JWT_SECRET} = require('../config/keys')
 const mongoose = require('mongoose')
 const User = mongoose.model("User")
 
@@ -9,7 +9,7 @@ module.exports = (req, res, next) => {
         return res.status(401).json({error: "You must be logged in"})
     }
     const token = authorization.replace("Bearer ","")
-    jwt.verify(token, JWTSECRET, (err, payload) => {
+    jwt.verify(token, JWT_SECRET, (err, payload) => {
         if(err) {
             return res.status(401).json({error: "You must be logged in"})
         }
